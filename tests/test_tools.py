ba2e085da0c2b96f29731508f7153978b739227e
lesyaolexs <lesyaolexs852@gmail.com>
2021-04-26 19:38:14 +0300

    Added requirements.txt

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -1,17 +1,18 @@
 import pytest
 import tools
 
+
 def test_monty_hall_problem_check_if_keep():
     """
     Verification of winnings if choose `keep` options in percentage.
     """
     wins_if_keep = tools.monty_hall_problem_check("keep", 10000).get("wins")*100//10000
-    assert wins_if_keep == 33
+    assert pytest.approx(wins_if_keep, abs=1) == 33
+
 
 def test_monty_hall_problem_check_if_change():
     """
        Verification of winnings if choose `change` options in percentage.
     """
     wins_if_change = tools.monty_hall_problem_check("change", 10000).get("wins")*100//10000
-    assert wins_if_change == 66
-
+    assert pytest.approx(wins_if_change, abs=1) == 66
